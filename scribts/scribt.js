690491ec9b780f506cdc5ab13bfbd2e6a446e5f8
Ahmed Mohamed <166655989+Ahmed-Mohamed-S@users.noreply.github.com>
2024-11-09 02:08:05 +0200

    first commit

---
+++ scribts/scribt.js
@@ -0,0 +1,6 @@
+var $grid = $('.xgrid').isotope({});
+
+$('.filter-button-group').on('click','button', function(){
+    var filterValue= $(this).attr('data-filter');
+    $grid.isotope({filter: filterValue});
+});
